66b34ee3cb009640d7cfbf37a51359a1e5c481ed
phylis13 <phylisnjeri54@gmail.com>
2023-03-19 16:02:08 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,16 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    new_movie = Movie.create(movie_params)
+    render json: new_movie, status: :created
+  rescue 
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description,:poster_url, :category, :discount, :female_director )
+  end
+
 end
